26580795d8ac121ea0b728585fc8f1c3bbed3985
jec1477 <jec1477@gmail.com>
2011-10-22 21:02:36 -0400

    more

--- db/migrate/20111008004710_change_resource_to_knowledge_kind.rb
+++ db/migrate/20111008004710_change_resource_to_knowledge_kind.rb
@@ -1,6 +1,7 @@
 class ChangeResourceToKnowledgeKind < ActiveRecord::Migration
   def up
-    rename_column :knowledges, :resource, :knowledge_kind # table name, old name, new name
+    add_column :knowledges, :knowledge_kind, :string
+    #rename_column :knowledges, :resource, :knowledge_kind # table name, old name, new name
   end
 
   def down
