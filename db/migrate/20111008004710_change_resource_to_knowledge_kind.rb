f7d858b35f8d9904ad78321ad6e1d4bddc631f1e
jec1477 <jec1477@gmail.com>
2011-10-22 21:06:43 -0400

    stuff

--- db/migrate/20111008004710_change_resource_to_knowledge_kind.rb
+++ db/migrate/20111008004710_change_resource_to_knowledge_kind.rb
@@ -1,7 +1,10 @@
 class ChangeResourceToKnowledgeKind < ActiveRecord::Migration
   def up
-    add_column :knowledges, :knowledge_kind, :string
-    #rename_column :knowledges, :resource, :knowledge_kind # table name, old name, new name
+    #for heroku
+    #add_column :knowledges, :knowledge_kind, :string
+    
+    #for development
+    rename_column :knowledges, :resource, :knowledge_kind # table name, old name, new name
   end
 
   def down
